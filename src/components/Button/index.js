f642bac691e8962a03e5585611c7682e15f0deed
hieuamaster <hieuanguyentrung@gmail.com>
2022-08-10 09:19:38 +0700

    Build Button Header #3

---
+++ src/components/Button/index.js
@@ -0,0 +1,58 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    children,
+    className,
+    disabled = false,
+    login_small = false,
+    login_big = false,
+    follow = false,
+    upload = false,
+    get_app = false,
+    leftIcon = false,
+    rightIcon = false,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disabled) {
+        delete props.onClick;
+    }
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.to = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: classNames,
+        login_small,
+        login_big,
+        follow,
+        get_app,
+        disabled,
+        upload,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,9 +1,10 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import { useState, useEffect } from 'react';
 
+import Button from '~/components/Button'
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopOverWrapper } from '../../../PopOver';
@@ -14,7 +15,7 @@ function Header() {
     const [searchResult, setSearchResult] = useState([]);
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([1, 2, 3]);
+            setSearchResult([]);
         }, 0);
     }, []);
 
@@ -43,7 +44,7 @@ function Header() {
                         <input placeholder="Search accounts and videos" spellCheck={false}></input>
                         <button className={cx('clear')}>
                             <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
-                        </button>
+                        </button> 
                         <FontAwesomeIcon className={cx('loading')} icon={faSpinner}></FontAwesomeIcon>
 
                         <button className={cx('search-btn')}>
@@ -51,7 +52,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button upload leftIcon={<FontAwesomeIcon  icon={faPlus} />}>Upload</Button>
+                    <Button login_small >Log in</Button>
+                </div>
             </div>
         </header>
     );
